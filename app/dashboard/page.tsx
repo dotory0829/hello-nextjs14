f911462c42b02a376a400adc5df0dd9991eca614
Ahn taeuk <dotory0829@naver.com>
2024-03-28 22:10:49 +0900

    first commit: nextjs14-docs-chapter6

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function DashboardRoot() {
+  return <p>DashboardRoot</p>;
+}
---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+import { lusitana } from './ui/font';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
